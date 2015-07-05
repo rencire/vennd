09cf9421e6b7dceed1e06fee4ae59690b2787c76
Rene <devproren@gmail.com>
2015-07-05 12:53:47 -0700

    cleaned up code

--- modules/dragzone.js
+++ modules/dragzone.js
@@ -5,25 +5,31 @@ export function makeDragzone(el, debug=false) {
   var firedEvents = [];
   var dragzone_enter = new Event('dragzone:enter', {"bubble": true, "cancelable":true});
   var dragzone_leave = new Event('dragzone:leave', {"bubble": true, "cancelable":true});
+  var timeout;
 
   el.addEventListener("dragenter", function(e){
+      var dt = e.dataTransfer;
       if (firedEvents.length === 0) {
-          dragzone_enter.dataTransfer = e.dataTransfer;
-          this.dispatchEvent(dragzone_enter);
+          dragzone_enter.dataTransfer = dt;
+          el.dispatchEvent(dragzone_enter);
       }
       firedEvents.push(e.target);
-      if(debug === true) console.log('dragenter', e.target, firedEvents);
+      if(debug === true) console.log('dragenter', e, firedEvents);
   }, false);
 
   el.addEventListener("dragleave", function(e){
       firedEvents = firedEvents.filter(function(elem) {
           return elem !== e.target;
       });
-      if(debug === true) console.log('dragleave',e.target, firedEvents);
+      if(debug === true) console.log('dragleave',e, firedEvents);
       if (firedEvents.length === 0) {
           dragzone_leave.dataTransfer = e.dataTransfer;
-          this.dispatchEvent(dragzone_leave);
+          el.dispatchEvent(dragzone_leave);
       }
+  }, false);
+
+  el.addEventListener("drop", function(e){
+    firedEvents = [];
   }, false);
 
 }
--- modules/main.js
+++ modules/main.js
@@ -200,57 +200,24 @@ function displayFileContents(file) {
     reader.readAsText(file);
 }    
 
-function dragenter(e) {
-    e.stopPropagation();
-    e.preventDefault();
-}
-
-function dragover(e) {
-    e.stopPropagation();
-    e.preventDefault();
-}
-
-function drop(e) {
-    e.stopPropagation();
-    e.preventDefault();
-
-    var dt = e.dataTransfer;
-    var files = dt.files;
-    handleFiles(files);
-}
-
-//hanldeFiles example from https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
-
-function setupFileDrop() {
-    // File Input
-    // var inputElement = document.getElementById("input");
-    // // var inputElement = d3.select("input");
-    // inputElement.addEventListener("change", handleFiles, false);
-    // function handleFiles() {
-    //     var fileList = this.files;
-    //     console.log(fileList);
-    // }
-
-    // Vanilla JS for binding drop events
-    var dropboxes;
-    dropboxes = document.getElementsByClassName("dropbox");
-
-    // See: "Why is NodeList not an Array?"
-    // https://developer.mozilla.org/en-US/docs/Web/API/NodeList
-    // dropboxes.forEach(function(e) {
-    //     e.addEventListener("dragenter", dragenter, false);
-    //     e.addEventListener("dragover", dragover, false);
-    //     e.addEventListener("drop", drop, false);
-    //
-    // });
-
-    for (var i = 0, len = dropboxes.length; i < len; i++) {
-        var input_ele = dropboxes[i];
-        input_ele.addEventListener("dragenter", dragenter, false);
-        input_ele.addEventListener("dragover", dragover, false);
-        input_ele.addEventListener("drop", drop, false);
-    }
-}
+// function dragenter(e) {
+//     e.stopPropagation();
+//     e.preventDefault();
+// }
+//
+// function dragover(e) {
+//     e.stopPropagation();
+//     e.preventDefault();
+// }
+//
+// function drop(e) {
+//     e.stopPropagation();
+//     e.preventDefault();
+//
+//     var dt = e.dataTransfer;
+//     var files = dt.files;
+//     handleFiles(files);
+// }
 
 // http://developers.arcgis.com/javascript/sandbox/sandbox.html?sample=exp_dragdrop
 
@@ -258,70 +225,33 @@ initializePage();
 renderBoard();
 setupFileDrop();
 
-// Since svg.drawboard is a child element of a Dragzone, we need to make sure dragenter and dragleave events propogate up
-function uploadDragEnter(e) {
-    e.preventDefault();
-    var dt = e.dataTransfer;
-    dt.dropEffect = 'copy';
-}
-
-
-function uploadDragLeave(e) {
-    e.preventDefault();
-    var dt = e.dataTransfer;
-    dt.dropEffect = 'move';
-}
-
-function uploadDragOver(e) {
-    // e.stopPropagation();
-    // e.preventDefault();
-    // var dt = e.dataTransfer;
-    // dt.dropEffect = 'copy';
-}
-
-function uploadDrop(e) {
-    e.stopPropagation();
-    e.preventDefault();
-}
-
-
-var drawboard = document.getElementsByClassName('drawboard')[0];
-drawboard.addEventListener("dragenter", uploadDragEnter, false);
-drawboard.addEventListener("dragover", uploadDragOver, false);
-drawboard.addEventListener("dragleave", uploadDragLeave, false);
-drawboard.addEventListener("drop", uploadDrop, false);
-
-
 // TODO: check if event contains files
 function containsFiles(e) {
     return true;
 }
-// NOTE: do not allow dropping files anywhere in body
-// TODO Since dragenter events are triggered on almost every div,
-// we should restrict adding div.drop-layer to an event from one event.target
 
 // NOTES:
 // - Currently, HTML5 dragenter and dragleave are similar to mousein and mouseout.
 // - Using a custom event to make dragenter and dragleave behave more like mouseenter and mouseleave.
 // - Custom solution also handles Firefox 38's weird behavior of double firing dragenter event...
-// - TODO Firefox 38 also fires our custom event when we reload the page.. Fix this!
 //
 // References:
 // - http://stackoverflow.com/questions/10253663/how-to-detect-the-dragleave-event-in-firefox-when-dragging-outside-the-window/10310815#10310815
-
-// Lets create a custom event to handle this.
-// NOTE on firefox,
 
 function handleDragzoneEnter(e) {
     e.stopPropagation();
     e.preventDefault();
     console.log('dragzone:enter');
-    // need to change effect of drag upon entering dropzone
-    // dt.effectAllowed = dt.dropEffect = 'none';
-    // insert drop-layer to drawboard
     views.show_msg.style.display = 'block';
 }
 
+function handleDragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('dragover');
+    var dt = e.dataTransfer;
+    dt.dropEffect = dt.effectAllowed = 'copy';
+}
 function handleDragzoneLeave(e) {
     e.stopPropagation();
     e.preventDefault();
@@ -329,32 +259,34 @@ function handleDragzoneLeave(e) {
     views.show_msg.style.display = '';
 }
 
+function handleDragzoneDrop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log('dragzone:drop');
+  views.show_msg.style.display = '';
+}
+
 var dragzone_ele = document.querySelector('.dragzone');
-makeDragzone(dragzone_ele,true);
+makeDragzone(dragzone_ele);
 
 dragzone_ele.addEventListener("dragzone:enter", handleDragzoneEnter, false);
+
+// change cursor on drag
+dragzone_ele.addEventListener("dragover", handleDragover, false);
 
 dragzone_ele.addEventListener("dragzone:leave", handleDragzoneLeave, false);
 
 
-
-// NOTE: Code below was used to troubleshoot an irreproducible issue. can safely delete.
-// document.querySelector('.files').addEventListener('dragenter', function(e) {
-//     console.log(e);
-//     console.log('is this serioiusly being fired?');
-// },false);
-
+// preventDefault uploading behavior when dropping files on dragzone_ele's child elements
 dragzone_ele.addEventListener("drop", function(e) {
     e.stopPropagation();
     e.preventDefault();
-    console.log('body drop');
+    console.log('drop');
+    views.show_msg.style.display = '';
+
+    // add circle to svg
+    console.log(e.dataTransfer);
+    if
+
 }, false);
 
-dragzone_ele.addEventListener("dragover", function(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  // console.log('document dragover');
-  // console.log(e.target);
-  var dt = e.dataTransfer;
-  dt.effectAllowed = dt.dropEffect = 'none';
-}, false);
