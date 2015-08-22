4209e46c5a5e81dd260da7fe6ffcb7f3ceab2d3b
Rene <devproren@gmail.com>
2015-08-22 00:59:37 -0700

    Completed initial batch of test cases for getIntersections()

--- src/circle.js
+++ src/circle.js
@@ -19,19 +19,28 @@ export function getAllIntersections(circles) {
  * Calculates the intersection points between two circles
  * @param {object} c1 - circle one
  * @param {object} c2 - circle two
- * @returns {Array} The two points that intersect a circle, or none.
+ * @returns {Array} List of points that intersect a circle, or an empty array if no intersection points exist.
+ *
+ * @throws Will throw an error if both arguments aren't present.
+ * @throws Will throw an error if both arguments aren't circles.
  *
  * @example
  * // returns:
  * // [
- * //   {x:25, y:15.101, parentCircles: [1,2]}, 
- * //   {x:25, y:15.101, parentCircles: [1,2]}, 
+ * //   {x:12.5, y:5.7, parentCircles: [1,2]}, 
+ * //   {x:12.5, y:14.330, parentCircles: [1,2]}, 
  * // ]
  *
- * var a = {id:1, center:{x:20, y:20}, radius: 49}, ... }
- * var b = {id:2, center:{x:30, y:20}, radius: 49}, ... }
- * getIntersections(a,b);
+ * var c1 = {id:1, center:{x:10, y:10}, radius: 5}, ... }
+ * var c2 = {id:2, center:{x:15, y:10}, radius: 5}, ... }
+ * getIntersections(c1,c2);
 */
-export function getIntersections() {
-
+export function getIntersections(a,b) {
+  if (a === undefined || b === undefined) {
+   throw "Not enough arguments";
+  }
+ // if a and b are not circles  {
+  // throw "Arguments are not circles"
+  //}
+  //
 }
--- src/circle.spec.js
+++ src/circle.spec.js
@@ -1,11 +1,11 @@
 'use strict';
 
-import {getAllIntersections} from './circle';
+import {getAllIntersections, getIntersections} from './circle';
 
 describe('get correct intersections for two circles with...', () => {
   it('same radius, translated', () => {
-    var c1 = {id:1, center:{x:20, y:20}, radius: 49};
-    var c2 = {id:2, center:{x:30, y:20}, radius: 49};
+    var c1 = {id:1, center:{x:20, y:20}, radius: 7};
+    var c2 = {id:2, center:{x:30, y:20}, radius: 7};
 
     var exp_p1 = {x:25, y: 15.101};
     var exp_p2 = {x:25, y: 24.899};
@@ -14,18 +14,18 @@ describe('get correct intersections for two circles with...', () => {
     var res_p1 = result[0];
     var res_p2 = result[1];
 
-    expect({x:res_p1.x y: res_p1.y}).toEqual(exp_p1);
+    expect({x: res_p1.x, y: res_p1.y}).toEqual(exp_p1);
     expect(res_p1.parentCircles).toContain(1);
     expect(res_p1.parentCircles).toContain(2);
 
-    expect({x:res_p2.x y: res_p2.y}).toEqual(exp_p2);
+    expect({x: res_p2.x, y: res_p2.y}).toEqual(exp_p2);
     expect(res_p2.parentCircles).toContain(1);
     expect(res_p2.parentCircles).toContain(2);
   });
 
   it('different radius, different center coordinates', () => {
-    var c1 = {id:3, center:{x:20, y:20}, radius: 25};
-    var c2 = {id:4, center:{x:25, y:25}, radius: 36};
+    var c1 = {id:3, center:{x:20, y:20}, radius: 5};
+    var c2 = {id:4, center:{x:25, y:25}, radius: 6};
 
     var result = getIntersections(c1,c2).sort();
     var ip1 = result[0];
@@ -42,9 +42,10 @@ describe('get correct intersections for two circles with...', () => {
     expect(ip2.parentCircles).toContain(4);
   });
 
-  it('duplicate circles', () => {
-    var c1 = {id:3, center:{x:20, y:20}, radius: 25};
-    expect(getIntersections(c1,c1)).toEqual([]);
+  it('duplicate dimensions', () => {
+    var c1 = {id:3, center:{x:20, y:20}, radius: 5};
+    var c2 = {id:8, center:{x:20, y:20}, radius: 5};
+    expect(getIntersections(c1,c2)).toEqual([]);
   });
 
   it('only one intersection point', () => {
@@ -57,36 +58,68 @@ describe('get correct intersections for two circles with...', () => {
     expect(ip1.parentCircles).toContain(7);
   });
 
+  it('no intersection points', () => {
+    var c1 = {id:42, center:{x:0, y:0}, radius: 5};
+    var c2 = {id:3, center:{x:20, y:20}, radius: 5};
+    
+    expect(getIntersections(c1,c2)).toEqual([]);
+  });
+
+  it('circle within another circle', () => {
+    var c1 = {
+      id: 1,
+      center: {x: 20, y: 20},
+      radius: 10
+    }; 
+
+    var c2 = {
+      id: 5,
+      center: {x: 20, y: 20},
+      radius: 5
+    };
+    expect(getIntersections(c1,c2)).toEqual([]);
+  });
+
+  it('invalid input will throw errors', () => {
+    var c1 = {id:4, center:{x:10, y:10}, radius:5};
+    expect(getIntersections(c1)).toThrowError("Not enough arguments");
+    expect(getIntersections(null,3)).toThrowError("Arguments are not circles");
+  });
+
 });
 
+
+
+//TODO fill in pending tests
 describe('get all correct intersections for...', () => {
-  it('empty circle list', () => {
-    let circles = [];
-    expect(getAllIntersections(circles)).toEqual([]); 
+
+  //TODO setup data to test
+  xit('multiple overlapping circles', () => {
+    // TODO add another circle to test
+    // let circles = [
+    //   {
+    //     id: 1,
+    //     center: {x: 100, y: 100},
+    //     radius: 50
+    //   }, 
+    //   {
+    //     id: 2,
+    //     center: {x: 105, y: 105},
+    //     radius: 50
+    //   }, 
+    // ];
+    //
+    // let points = [
+    //   {x:95.886, y:109.114, parentCircles: [1,2]},
+    //   {x:109.114, y:95.886, parentCircles: [1,2]},
+    // ];
+    // expect(getAllIntersections(circles)).toEqual(points); 
+
   });
 
-  it('duplicate circles', () => {
-    let circles = [
-      {
-        id: 1,
-        center: {x: 100, y: 100},
-        radius: 50
-      }, 
-      {
-        id: 2,
-        center: {x: 100, y: 100},
-        radius: 50
-      }, 
-    ];
-    expect(getAllIntersections(circles)).toEqual([]); 
-  });
-
-  xit('circles within other circles', () => {
-
-  });
-
-  it('circles that do not overlap', () => {
-    let circles = [
+  //TODO setup data to test 'exp_points', 'points', etc.
+  xit('multiple circles that do not all overlap', () => {
+    var circles = [
       {
         id: 1,
         center: {x: 100, y: 100},
@@ -97,14 +130,29 @@ describe('get all correct intersections for...', () => {
         center: {x: 150, y: 150},
         radius: 50
       }, 
+      {
+        id: 3,
+        center: {x: 333, y: 333},
+        radius: 33
+      }
     ];
 
-    let points = [];
+    var points = [];
+    var exp_points = [];
+
+    expect(getAllIntersections(circles)).toEqual(exp_points); 
+  });
+
+  xit('multiple circles with no overlap', () => {
+  });
+
+  it('empty circle list', () => {
+    let circles = [];
     expect(getAllIntersections(circles)).toEqual([]); 
   });
 
-  it('two overlapping circles', () => {
-    let circles = [
+  it('duplicate circles', () => {
+    var circles = [
       {
         id: 1,
         center: {x: 100, y: 100},
@@ -112,23 +160,48 @@ describe('get all correct intersections for...', () => {
       }, 
       {
         id: 2,
-        center: {x: 105, y: 105},
+        center: {x: 100, y: 100},
         radius: 50
       }, 
     ];
+    expect(getAllIntersections(circles)).toEqual([]); 
+  });
 
-    let points = [
-      {x:95.886, y:109.114, parentCircles: [1,2]},
-      {x:109.114, y:95.886, parentCircles: [1,2]},
+  it('circles w/ duplicate dimensions', () => {
+    var c1 = {id:3, center:{x:20, y:20}, radius: 5};
+    var c2 = {id:8, center:{x:20, y:20}, radius: 5};
+    var c3 = {id:8, center:{x:20, y:20}, radius: 5};
+    expect(getAllIntersections([c1,c2, c3])).toEqual([]);
+  });
+
+  it('circles within other circles', () => {
+    var circles = [
+      {
+        id: 1,
+        center: {x: 10, y: 10},
+        radius: 10
+      }, 
+      {
+        id: 5,
+        center: {x: 10, y: 10},
+        radius: 5
+      }, 
+      {
+        id: 9,
+        center: {x: 10, y: 10},
+        radius: 2
+      }, 
     ];
-    expect(getAllIntersections(circles)).toEqual(points); 
+    expect(getAllIntersections(circles)).toEqual([]);
+  });
 
+  xit('circle1 intersect w/ circle2, both within circle3', () => {
   });
 
   it('invalid input', () => {
-    expect(getAllIntersections(null)).toEqual(null); 
-    expect(getAllIntersections(123)).toEqual(null);
-    expect(getAllIntersections('abcd')).toEqual(null); 
+    expect(getAllIntersections(null)).toThrowError('Argument is not a list'); 
+    expect(getAllIntersections(123)).toThrowError('Argument is not a list'); 
+    expect(getAllIntersections('abcd')).toThrowError('Argument is not a list'); 
   });
 });
 
