f39eda506fecfa52ccc669e82f3529f3e47557c4
Rene <devproren@gmail.com>
2015-09-22 23:43:39 -0700

    added notes about motivation behind dragzone.js

--- modules/dragzone.js
+++ modules/dragzone.js
@@ -1,6 +1,19 @@
 // Eric Ren 2015
 // dragzone.js
 //
+// Drag events that actually make sense.
+//
+// By default, 'dragenter' event always fires when entering an element.
+// Conversely, 'dragleave' event always fires when leaving an element.
+//
+// If we want a dragzone composed of a hiearchy of elements, then dragging over the hiearchy will result in
+// a LOT of 'dragenter/dragleave' events as you enter and leave elements. 
+//
+// What we really want is to designate this hiearcy of elements as a 'dragzone', so that whenever we enter 
+// this zone, an 'enter' event fires only once, and a 'leave' event fires only once when we drag out of the
+// parent element. Dragging over child elements within the parent element will NOT fire all those extraneous
+// 'dragenter/dragleave' events.
+//
 export function makeDragzone(el, debug=false) {
   var firedEvents = [];
   var dragzone_enter = new Event('dragzone:enter', {"bubble": true, "cancelable":true});
